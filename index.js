6c00d6796179efeda8e4b61e8f16a849ff96e86c
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-12 20:19:32 +0000

    Added account class, got rid of global balance variable

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+//let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
+/*
+class Transaction {
+} */
 
+class Withdrawal extends Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance -= this.amount;
+  }
+}
 
+class Deposit extends Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
+const myAccount = new Account("Daniel James")
+console.log(myAccount);
 t1 = new Withdrawal(50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
@@ -26,4 +42,9 @@ t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
+t3 = new Deposit(100);
+t3.commit();
+console.log('Transaction 3: ', t3)
+
 console.log('Balance:', balance);
+
